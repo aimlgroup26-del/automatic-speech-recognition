0b685e1c3b8299be7b92f160a843b4c762d4d727
sanath-madhav <aimlgroup26@gmail.com>
2026-06-27 11:21:41 +0530

    model data cleaning

--- src/asr.py
+++
@@ -1,7 +0,0 @@
-import whisper
-
-
-def transcribe(audio_path: str, model_name: str = "base") -> str:
-    model = whisper.load_model(model_name)
-    result = model.transcribe(audio_path)
-    return result["text"]
--- src/utils.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def validate_audio_file(path: str) -> bool:
-    supported = {".wav", ".mp3", ".m4a", ".flac"}
-    return os.path.isfile(path) and os.path.splitext(path)[1].lower() in supported
